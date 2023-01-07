d4947aac42e4e625d9832f1c758bf7369e6f5288
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-01-07 15:06:03 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyPortal Page</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+</head>
+<body>
+    <h1>
+        <a href="index.php">Sam's Portal Page</a>
+    </h1>
+    <h2>The navigation below represents our BIG assignments.</h2>
+    <div id="wrapper">
+        <nav>
+            <ul>
+                <li><a href="">Switch</a></li>
+                <li><a href="">Troubleshoot</a></li>
+                <li><a href="">Calculator</a></li>
+                <li><a href="">Email</a></li>
+                <li><a href="">Database</a></li>
+                <li><a href="">Gallery</a></li>
+            </ul>
+        </nav>
+
+        <main>
+        <h2>About Sam</h2>
+        <img class="right" src="images/me.jpg" width="300px" height="350px" alt="Picture of me">
+        <p>
+            A paragraph about me Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit officia officiis libero quo reiciendis nobis autem accusamus est quas.
+        </p>
+        </main>
+
+        <aside>
+        <h2>Weekly Class Exercises</h2>
+        <h3>Week 2</h3>
+        <ul>
+            <li><a href="var.php">var.php</a></li>
+            <li><a href="">var2.php</a></li>
+            <li><a href="">something.php</a></li>
+            <li><a href="">something2.php</a></li>
+        </ul>
+        </aside>
+
+    </div>
+</body>
+</html>
