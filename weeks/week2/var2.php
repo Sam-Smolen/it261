1ff50160dd546d4892026957b4cb836eb66101de
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-01-10 18:38:19 -0800

    added arrays to var2

--- weeks/week2/var2.php
+++ weeks/week2/var2.php
@@ -55,7 +55,70 @@ $product = 120; // is our integer
 $quantity = 5;
 $total = $product * $quantity;
 $total *= 1.098;
+echo 'We have a total of <b>$'.number_format($total, 2).'</b> dollars';
+echo '<h3>Our second preset function is our date function</h3>';
+echo date('Y');
 echo '<br>';
+echo date('l');
+echo '<br>';
+echo date("l jS \of F Y h:i:s A");
 
 echo '<br>';
+echo date("l jS \of F Y h:i A");
+echo '<br>';
+date_default_timezone_set ('America/Los_Angeles');
+echo '<br>';
+echo date("l jS \of F Y h:i A");
+
+echo '<h3>Time for an array</h3>';
+echo '<h4>Below is an index array</h4>';
+// can we echo an array ??? no we cannot
+$fruit[] = 'bananas'; // 0
+$fruit[] = 'cherries'; // 1
+$fruit[] = 'melon'; // 2
+$fruit[] = 'kiwi';
+$fruit[] = 'oranges';
+$fruit[] = 'apples';
+
+echo $fruit;
+echo '<br>';
+echo $fruit[2];
+
+$fruit = array (
+   'bananas', 
+   'cherries',
+   'melon',
+   'kiwi',
+   'oranges',
+   'apples'
+);
+
+$fruit = [
+    'bananas', 
+   'cherries',
+   'melon',
+   'kiwi',
+   'oranges',
+   'apples'
+];
+
+echo '<br>';
+print_r($fruit);
+echo '<br>';
+var_dump($fruit);
+
+echo '<h3>Now we have an associative array</h3>';
+
+$nav = array(
+    'index.php' => 'Home', // index is the key and home is the value
+    'about.php' => 'About',
+    'daily.php' => 'Daily',
+    'project.php' => 'Project',
+    'contact.php' => 'Contact',
+    'gallery.php' => 'Gallery'
+);
+
+echo '<pre>';
+var_dump($nav);
+echo '<pre>';
 
