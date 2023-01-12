a871979a812f646b7e4d198709ea7b8c30ae54f6
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-01-11 17:45:35 -0800

    added paths for currency/currecy_logic

--- index.php
+++ index.php
@@ -38,8 +38,8 @@
         <ul>
             <li><a href="weeks/week2/var.php">var.php</a></li>
             <li><a href="weeks/week2/var2.php">var2.php</a></li>
-            <li><a href="">something.php</a></li>
-            <li><a href="">something2.php</a></li>
+            <li><a href="weeks/week2/currency.php">currency.php</a></li>
+            <li><a href="weeks/week2/currency_logic.php">currency_logic.php</a></li>
         </ul>
         </aside>
         <img class="left "src="images/mamp.png" width="1000px" height="800px" alt="mamp">
