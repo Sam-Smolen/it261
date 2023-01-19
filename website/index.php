5c65c8d7946388c85316166d5eb9563e40f25cf2
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-01-19 11:21:13 -0800

    add includes

---
+++ website/index.php
@@ -0,0 +1,31 @@
+<?php
+include('./includes/header.php');
+?>
+
+
+<div id="wrapper">
+<div id="hero">
+<img src="images/twelve.jpg" alt="Twelve is greater than 3">
+</div>
+<!-- end hero -->
+<main>
+<h1>Welcome to our Web App Programming Class!</h1>
+<h2>We are going to learn PHP!</h2>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque veniam facilis animi perspiciatis voluptates, odio eos impedit adipisci voluptate, facere expedita eligendi magni mollitia modi esse nihil dicta voluptatibus cum soluta, ad sed corporis doloremque labore. Sunt inventore culpa rem aut error laboriosam in, accusantium voluptates eveniet reprehenderit maiores.
+</p>
+<h2>Another Headline 2!</h2>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente sint aut saepe labore illo consequatur deleniti corrupti vitae quisquam! Quasi impedit tempora autem! Quidem, excepturi.
+</p>
+</main>
+<aside>
+<h3>This is our headline three in our beautiful aside</h3>
+<p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam voluptate perspiciatis sequi culpa dolor quas, id natus laborum neque accusamus veritatis libero commodi vero et. Fugit quaerat esse perferendis recusandae.
+</p>
+</aside>
+</div>
+<!-- end wrapper -->
+<?php
+include('./includes/footer.php');
