c69af452b925fc376fc54f2b860a202de5b55e02
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-01-22 13:40:39 -0800

    add week 3 exercises

--- index.php
+++ index.php
@@ -45,6 +45,16 @@
             <li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
             <li><a href="weeks/week2/heredoc.php">heredoc.php</a></li>
         </ul>
+        <h3 class="display__3" style="padding-top:5px;">Week 3</h3>
+        <ul class="weekly__exercises">
+            <li><a href="weeks/week3/about.php">about.php</a></li>
+            <li><a href="weeks/week3/date.php">date.php</a></li>
+            <li><a href="weeks/week3/for-each.php">for-each.php</a></li>
+            <li><a href="weeks/week3/for-loop.php">for-loop.php</a></li>
+            <li><a href="weeks/week3/if.php">if.php</a></li>
+            <li><a href="weeks/week3/index.php">index.php</a></li>
+            <li><a href="weeks/week3/switch.php">switch.php</a></li>
+        </ul>
         </aside>
         <footer>
              <p><small>&copy; 2022 by 
