ac2ccff51230b5e29d3ceb3bedeed0b240363457
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-02-06 15:24:07 -0800

    add link to calculator.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <ul>
                 <li><a href="website/daily.php">Switch</a></li>
                 <li><a href="weeks/week4/troubleshoot.php">Troubleshoot</a></li>
-                <li><a href="">Calculator</a></li>
+                <li><a href="weeks/week5/calculator.php">Calculator</a></li>
                 <li><a href="">Email</a></li>
                 <li><a href="">Database</a></li>
                 <li><a href="">Gallery</a></li>
