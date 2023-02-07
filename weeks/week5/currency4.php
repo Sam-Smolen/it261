73ca09f9cc05b04706ca0039f0e2bc50989b0608
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-02-06 17:16:07 -0800

    update title to sticky 4

--- weeks/week5/currency4.php
+++ weeks/week5/currency4.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Currency 3 sticky form</title>
+    <title>Currency 4 sticky form</title>
     <link href="css/currency.css" rel="stylesheet" type="text/css">
 </head>
 <body>
