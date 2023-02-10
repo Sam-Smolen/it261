31f9909e90ef439a672508cd3898c360191edd96
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-02-09 17:50:32 -0800

    update styles/ make form sticky

--- weeks/week6/form.php
+++ weeks/week6/form.php
@@ -8,35 +8,59 @@
     <link rel="stylesheet" href="css/styles.css" type="text/css">
 </head>
 <body>
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
     <fieldset>
     <legend>Contact Sam</legend>
     
     <label>First Name</label>
-    <input type="text" name="first_name">
+    <input type="text" name="first_name" value="<?php if(isset($_POST['first_name'])) echo htmlspecialchars($_POST['first_name']) ;?>">
 
     <label>Last Name</label>
-    <input type="text" name="last_name">
+    <input type="text" name="last_name" value="<?php if(isset($_POST['last_name'])) echo htmlspecialchars($_POST['last_name']) ;?>">
 
     <label>Email</label>
-    <input type="email" name="email">
+    <input type="email" name="email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']) ;?>">
     
     <label>Gender</label>
     <ul>
     <!-- will come back to gender once we will br adding the sticky part of the form -->
     <li>
-    <input type="radio" name="gender" value="male">
+    <input type="radio" name="gender" value="female" <?php if(isset($_POST['gender']) && $_POST['gender'] == 'female') echo 'checked="checked"';?>>Female
+    </li>
+
+    <li>
+    <input type="radio" name="gender" value="male" <?php if(isset($_POST['gender']) && $_POST['gender'] == 'male') echo 'checked="checked"';?>>Male
+    </li>
+
+    <li>
+    <input type="radio" name="gender" value="neither" <?php if(isset($_POST['gender']) && $_POST['gender'] == 'neither') echo 'checked="checked"';?>>Neither
     </li>
     </ul>
 
     <label>Phone</label>
-    <input type="tel" name="phone">
+    <input type="tel" name="phone" value="<?php if(isset($_POST['phone'])) echo htmlspecialchars($_POST['phone']) ;?>">
 
     <label>Favorite Wines</label> 
     <ul>
         <!-- we will return to the wines portion to add additional wines when we are adding the sticky part of our form -->
     <li>
-    <input type="checkbox" name="wines[]" value="cab">Cabernet
+    <input type="checkbox" name="wines[]" value="cab" <?php if(isset($_POST['wines']) && in_array('cab', $wines)) ;?>>Cabernet
+    </li>
+
+    <li>
+    <input type="checkbox" name="wines[]" value="merlot" <?php if(isset($_POST['wines']) && in_array('merlot', $wines)) ;?>>Merlot
+    </li>
+
+    <li>
+    <input type="checkbox" name="wines[]" value="syrah" <?php if(isset($_POST['wines']) && in_array('syrah', $wines)) ;?>>Syrah
+    </li>
+
+    <li>
+    <input type="checkbox" name="wines[]" value="red" <?php if(isset($_POST['wines']) && in_array('red', $wines)) ;?>>Redblend
+    </li>
+
+    <li>
+    <input type="checkbox" name="wines[]" value="malbec" <?php if(isset($_POST['wines']) && in_array('malbec', $wines)) ;?>>Malbec
     </li>
 
     </ul>
@@ -45,22 +69,34 @@
         <!-- we will return to options to add additional options when we are making our form sticky -->
     <option value="" NULL>Select one</option>
 
+    <option value="nw" <?php if(isset($_POST['regions']) && $_POST['regions'] == 'nw') echo 'selected="unselected"';?>>Northwest!</option>
+
+    <option value="sw" <?php if(isset($_POST['regions']) && $_POST['regions'] == 'sw') echo 'selected="selected"' ;?>>Southwest!</option>
+
+    <option value="mw" <?php if(isset($_POST['regions']) && $_POST['regions'] == 'mw') echo 'selected="selected"' ;?>>Midwest!</option>
+
+    <option value="ec" <?php if(isset($_POST['regions']) && $_POST['regions'] == 'ec') echo 'selected="selected"' ;?>>East Coast!</option>
+
+    <option value="ne" <?php if(isset($_POST['regions']) && $_POST['regions'] == 'ne') echo 'selected="selected"' ;?>>Northeast!</option>
+
 
     </select>
 
     <label>Comments</label>
-    <textarea name="comments"></textarea>
+    <textarea name="comments"><?php if(isset($_POST['comments'])) echo htmlspecialchars($_POST['comments']) ;?></textarea>
 
 
     <label>Privacy</label>
     <ul>
 
     <li>
-    <input type="radio" name="privacy" value="yes">Privacy
+    <input type="radio" name="privacy" value="yes" <?php if(isset($_POST['privacy']) && $_POST['privacy'] == 'yes') echo 'checked="checked"' ;?>>Privacy
     </li>
     </ul>
 
     <input type="submit" value="Send it!">
+
+
 
 
 
