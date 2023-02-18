19294af3fd12ee9dc7e9bbcf3366b6cf73626ddd
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-02-18 11:35:17 -0800

    update phone validation

--- weeks/week7/contact.php
+++ weeks/week7/contact.php
@@ -40,11 +40,16 @@ if(empty($_POST['email'])) {
     $email = $_POST['email'];
 }
 
-if(empty($_POST['phone'])) {
-    $phone_err = '';
-} else {
-    $phone = $_POST['phone']
-}
+if(empty($_POST['phone'])) { // if empty, type in your number
+    $phone_err = 'Your phone number please!';
+    } elseif(array_key_exists('phone', $_POST)){
+    if(!preg_match('/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/', $_POST['phone']))
+    { // if you are not typing the requested format of xxx-xxx-xxxx, display Invalid format
+    $phone_err = 'Invalid format!';
+    } else{
+    $phone = $_POST['phone'];
+    } // end else
+    } // end main if
 
 if(empty($_POST['toppings'])) {
     $toppings_err = '';
