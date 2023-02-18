73c56e9841cf4da9a1c0fcd1fe94862fc9d691a1
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-02-18 13:12:56 -0800

    make contact form sticky

--- weeks/week7/contact.php
+++ weeks/week7/contact.php
@@ -16,26 +16,25 @@ $email_err = '';
 $phone_err = '';
 $toppings_err = '';
 $size_err = '';
-$comments_err = '';
 $privacy_err = '';
 
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 if(empty($_POST['first_name'])) {
-    $first_name_err = '';
+    $first_name_err = 'Please enter your first name!';
 } else {
     $first_name = $_POST['first_name'];
 }
 
 if(empty($_POST['last_name'])) {
-    $last_name_err = '';
+    $last_name_err = 'Please enter your last name!';
 } else {
     $last_name = $_POST['last_name'];
 }
 
 if(empty($_POST['email'])) {
-    $email_err = '';
+    $email_err = 'Please provide an email address!';
 } else {
     $email = $_POST['email'];
 }
@@ -52,25 +51,19 @@ if(empty($_POST['phone'])) { // if empty, type in your number
     } // end main if
 
 if(empty($_POST['toppings'])) {
-    $toppings_err = '';
+    $toppings_err = 'You must choose at least one topping!';
 } else {
     $toppings = $_POST['toppings'];
 }
 
-if(empty($_POST['size'])) {
-    $size_err = '';
+if($_POST['size'] == NULL) {
+    $size_err = 'Select a size';
 } else {
     $size = $_POST['size'];
 }
 
-if(empty($_POST['comments'])) {
-    $comments_err = '';
-} else {
-    $comments = $_POST['comments']
-}
-
 if(empty($_POST['privacy'])) {
-    $privacy_err = '';
+    $privacy_err = 'Please agree to our privacy policy!';
 } else {
     $privacy = $_POST['privacy'];
 }
@@ -102,69 +95,73 @@ if(empty($_POST['privacy'])) {
     <legend>Complete Your Order</legend>
 
     <label>First Name</label>
-    <input type="text" name="first_name">
-
+    <input type="text" name="first_name" value="<?php if(isset($_POST['first_name'])) echo htmlspecialchars($_POST['first_name']);?>">
+    <span class="error"><?php echo $first_name_err ;?></span>    
     <label>Last Name</label>
-    <input type="text" name="last_name">
-
+    <input type="text" name="last_name" value="<?php if(isset($_POST['last_name'])) echo htmlspecialchars($_POST['last_name']);?>">
+    <span class="error"><?php echo $last_name_err ;?></span>
     <label>Email</label>
-    <input type="email" name="email">
-
+    <input type="email" name="email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">
+    <span class="error"><?php echo $email_err ;?></span>
     <label>Phone</label>
-    <input type="tel" name="phone" placeholder="xxx-xxx-xxxx">
-
+    <input type="tel" name="phone" placeholder="xxx-xxx-xxxx" value="<?php if(isset($_POST['phone'])) echo htmlspecialchars($_POST['phone']);?>">
+    <span class="error"><?php echo $phone_err ;?></span>
+    
     <label>Toppings</label>
     <ul>
     <li>
-    <input type="checkbox" name="toppings[]" value="pep">Pepperoni
+    <input type="checkbox" name="toppings[]" value="pep" <?php if(isset($_POST['toppings']) && in_array('pep', $toppings)) echo 'checked="checked"';?>>Pepperoni
     </li>
     <li>
-    <input type="checkbox" name="toppings[]" value="olives">Olives
+    <input type="checkbox" name="toppings[]" value="olives" <?php if(isset($_POST['toppings']) && in_array('olives', $toppings)) echo 'checked="checked"';?>>Olives
     </li>
     <li>
-    <input type="checkbox" name="toppings[]" value="gpep">Green Peppers
+    <input type="checkbox" name="toppings[]" value="gpep" <?php if(isset($_POST['toppings']) && in_array('gpep', $toppings)) echo 'checked="checked"';?>>Green Peppers
     </li>
     <li>
-    <input type="checkbox" name="toppings[]" value="rpep">Red Peppers
+    <input type="checkbox" name="toppings[]" value="rpep" <?php if(isset($_POST['toppings']) && in_array('rpep', $toppings)) echo 'checked="checked"';?>>Red Peppers
     </li>
     <li>
-    <input type="checkbox" name="toppings[]" value="onions">Onions
+    <input type="checkbox" name="toppings[]" value="onions" <?php if(isset($_POST['toppings']) && in_array('onions', $toppings)) echo 'checked="checked"';?>>Onions
     </li>
     <li>
-    <input type="checkbox" name="toppings[]" value="sausage">Sausage
+    <input type="checkbox" name="toppings[]" value="sausage" <?php if(isset($_POST['toppings']) && in_array('sausage', $toppings)) echo 'checked="checked"';?>>Sausage
     </li>
     <li>
-    <input type="checkbox" name="toppings[]" value="mushrooms">Mushrooms
+    <input type="checkbox" name="toppings[]" value="mushrooms" <?php if(isset($_POST['toppings']) && in_array('mushrooms', $toppings)) echo 'checked="checked"';?>>Mushrooms
     </li>
     </ul>
-    
+    <span class="error"><?php echo $toppings_err;?></span>
+
     <label>Choose Size</label>
     <select name="size">
 
-    <option value="" NULL>Select one</option>
+    <option value="" NULL <?php if(isset($_POST['size']) && $_POST['size'] == NULL) echo 'selected="unselected"';?>>Select one</option>
 
-    <option value="small">Small</option>
+    <option value="small" <?php if(isset($_POST['size']) && $_POST['size'] == 'small') echo 'selected="selected"';?>>Small</option>
 
-    <option value="medium">Medium</option>
+    <option value="medium" <?php if(isset($_POST['size']) && $_POST['size'] == 'medium') echo 'selected="selected"';?>>Medium</option>
 
-    <option value="large">Large</option>
+    <option value="large" <?php if(isset($_POST['size']) && $_POST['size'] == 'large') echo 'selected="selected"';?>>Large</option>
 
-    <option value="x-large">Extra Large</option>
+    <option value="x-large" <?php if(isset($_POST['size']) && $_POST['size'] == 'x-large') echo 'selected="selected"';?>>Extra Large</option>
     
     </select>
+    <span class="error"><?php echo $size_err ;?></span>
 
-    <label>Comments</label>
+    <label>Additional Comments or Instructions (not required.)</label>
     <textarea name="comments"></textarea>
 
     <label>Privacy</label>
     <ul>
         <li>
-        <input type="radio" name="privacy" value="yes">Privacy
+        <input type="radio" name="privacy" value="yes" <?php if(isset($_POST['privacy']) && $_POST['privacy'] == 'yes') echo 'checked="checked"';?>>Privacy
         </li>
     </ul>
+    <span class="error"><?php echo $privacy_err ;?></span>
     <input type="submit" value="Place my Order!">
 
-    <input type="button" value="reset">
+    <input type="button" onclick="window.location.href='<?php echo $_SERVER['PHP_SELF'] ;?>'" value="reset">
 
     </fieldset>
     </form>
