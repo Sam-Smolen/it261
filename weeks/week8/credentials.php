2a4d94c064a56c47629417aaab040d09aa3020d3
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-02-22 15:06:41 -0800

    add people.php and images for week8

--- weeks/week8/credentials.php
+++ weeks/week8/credentials.php
@@ -2,3 +2,8 @@
 
 //samsmlen_school_projects - name of my database
 //samsmlen_school_projects_user - name of user
+
+define('DB_NAME', 'samsmlen_school_projects');
+define('DB_USER', 'samsmlen_school_projects_user');
+define('DB_PASSWORD', 'Basilisk312');
+define('DB_HOST', 'localhost');
---
+++ weeks/week8/people.php
@@ -0,0 +1,50 @@
+<?php
+include('config.php');
+
+$sql = 'SELECT * FROM people';
+// now we have to connect to our database
+
+//$iConn = mysqli_connect(DB_NAME , DB_USER , DB_PASSWORD , DB_HOST) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));;
+
+$iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));;
+
+$result = mysqli_query($iConn, $sql) or die(myError(__FILE__,__LINE__,mysqli_error($iConn)));;
+
+// we are asking if the number of rows is greater than 0
+
+
+if(mysqli_num_rows($result) > 0)  {
+// we will be reading those rows with a while loop
+while($row = mysqli_fetch_assoc($result)) {
+
+echo '
+<h2>Information about: '.$row['first_name'].'</h2>
+<ul>
+<li><b>First Name:</b> '.$row['first_name'].'</li>
+<li><b>Last Name:</b> '.$row['last_name'].'</li>
+<li><b>Birthdate:</b> '.$row['birthdate'].'</li>
+</ul>
+<p>For more information about '.$row['first_name'].', click <a href="people-view.php?id='.$row['people_id'].'">here</a></p>
+';
+
+
+
+
+
+} // end while
+
+
+$photos[0] = 'photo1';
+$photos[1] = 'photo2';
+$photos[2] = 'photo3';
+$photos[3] = 'photo4';
+$photos[4] = 'photo5';
+
+$i = rand(0, 4);
+
+$selected_image = ''.$photos[$i].'.jpg ';
+echo '<img src="./images/'.$selected_image.'" alt="'.$photos[$i].'" >';
+
+
+
+} // end if number of rows is greater than 0
