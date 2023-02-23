4922321a3db0b4acc2f561a10ec24d169104a631
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-02-23 11:19:59 -0800

    update footer.php and styles for footer

--- weeks/week8/includes/footer.php
+++ weeks/week8/includes/footer.php
@@ -1,18 +1,16 @@
 <footer>
-    <ul>
-        <li>Copyright &copy;
-            2022</li>
-        <li>All Rights Reserved</li>
-        <li><a href="../index.php">Web Design by YOUR NAME</a></li>
-        <li><a id="html-checker" href="">HTML Validation</a></li>
-        <li><a id="css-checker" href="">CSS Validation</a></li>
-        </ul>
-        
-        <script>
-                document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
-                document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
-        </script>
-</footer>
+             <p><small>&copy; 2022 All Rights Reserved  
+                 <a href="contact.php">Web Design By Sam Smolen</a>, <a id="html-checker" href="#top" target="_blank">Check HTML</a> ~
+      <a id="css-checker" href="#" target="_blank">Check CSS</a>
+            </p>
+           </footer>
+    </div>
+
+    <script>
+  document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+             
+  document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+</script>
 
 </body>
 </html>
