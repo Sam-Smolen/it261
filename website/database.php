12189e23aff079442a5871605405e6695210739c
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-03-03 12:23:51 -0800

    test db configuration

--- website/database.php
+++ website/database.php
@@ -4,5 +4,23 @@ include('config.php');
 include('./includes/header.php');
 
 ?>
+<div id="wrapper">
+<main> 
+<h1>My week 9 Database HW! <br>Scary Movies!</h1>
+<?php 
 
-<h1>HEllo world</h1>
+
+?>
+
+</main>
+
+<aside>
+
+<h3>This is my aside</h3>
+<aside>
+    <h3>I will be displaying my random images here</h3>
+</aside>
+</div>
+<?php
+include('./includes/footer.php');
+?>
