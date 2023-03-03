4938012495206fce77c0f827130d0bbdd5b44abd
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-03-03 11:20:54 -0800

    add login.php/update server.php

--- weeks/week9/config.php
+++ weeks/week9/config.php
@@ -36,3 +36,5 @@ if(defined('DEBUG') && DEBUG)
     
     
 }
+
+?>
--- weeks/week9/credentials.php
+++ weeks/week9/credentials.php
@@ -7,3 +7,5 @@ define('DB_NAME', 'samsmlen_school_projects');
 define('DB_USER', 'samsmlen_school_projects_user');
 define('DB_PASSWORD', 'Basilisk312');
 define('DB_HOST', 'localhost');
+
+?>
