f5b3a1f1ce3a7d750f95df7d70c0d28eaf0a4dc0
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-03-04 12:44:00 -0800

    add about page/ more css

---
+++ website/about.php
@@ -0,0 +1,16 @@
+<?php
+include('config.php');
+include('./includes/db_header.php');
+?>
+
+<div id="wrapper">
+<div class="about-view">
+    <h1>Welcome to my about page!</h1>
+    <h2>My Scary Movies Table!</h2>
+    <img src="./images/movies_db.png" alt="movies table screenshot">
+</div>
+</div>
+
+<?php
+include('./includes/footer.php');
+?>
