dec0e98173168907bd94ee83dc43f68f2ff5154d
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-03-10 12:14:42 -0800

    update index.php/test login

--- weeks/week9/index.php
+++ weeks/week9/index.php
@@ -6,7 +6,7 @@ session_start();
 
 include('config.php');
 
-// did the user log in correctly
+// did the user log in correctly, and if not, the user will receive a message and will be directed back to the log in page
 
 if(!isset($_SESSION['username'])) {
     $_SESSION['msg'] = 'You must login first';
@@ -20,3 +20,36 @@ if(isset($_GET['logout'])) {
 }
 
 //include('./includes/header.php');
+
+// logic - is our session success set?
+
+if(isset($_SESSION['success'])) :?>
+<div class="success">
+    <h3>
+        <?php echo $_SESSION['success'];
+        unset($_SESSION['success']); ?>
+    </h3>
+</div>
+<!-- close success div -->
+<?php endif; ?>
+
+<?php // is our wonderful session username set???
+
+if(isset($_SESSION['username'])) :?>
+<div class="welcome-logout">
+<h3> Welcome, 
+<?= $_SESSION['username'] ?>
+</h3> 
+<p><a href="index.php?logout='1'">Logout</a></p>
+</div>
+<!-- close welcome loogout -->
+<?php endif; ?>
+
+<div id="wrapper">
+<h1>Welcome to our home page!!!</h1>
+
+
+</div>
+
+<?php
+//include('./includes/footer.php');
