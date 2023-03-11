76d4b69b52a7d5ac19ff3681947c8583e1c1606a
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-03-11 11:09:10 -0800

    more css

--- weeks/week9/errors.php
+++ weeks/week9/errors.php
@@ -2,11 +2,12 @@
 // errors page which will be using a for each loop
 // we are going to be counting errors
 // if we have more than ZERO errors, not a good thing, we will need to echo our message
+include('./includes/header.php');
 
 if(count($errors) > 0) : ?>
 <div class="error">
 <?php foreach($errors as $error) : ?>
-<p>
+<p class="errors">
 <?= $error ?>
 </p>
 <?php endforeach; ?>
